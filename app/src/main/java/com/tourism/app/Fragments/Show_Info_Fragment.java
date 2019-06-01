fdc00dff661852ba519c0d69526020a36eb6e7a3
wasdfra <wasdfra@gmail.com>
2019-06-01 15:41:35 +0100

    Implemented TTS(Text to Speech) on info

--- app/src/main/java/com/tourism/app/Fragments/Show_Info_Fragment.java
+++ app/src/main/java/com/tourism/app/Fragments/Show_Info_Fragment.java
@@ -1,27 +1,90 @@
 package com.tourism.app.Fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tourism.app.R;
 
-public class Show_Info_Fragment extends Fragment {
+import java.util.Locale;
+
+public class Show_Info_Fragment extends Fragment  {
+
 
     public static Show_Info_Fragment newInstance() {
         return new Show_Info_Fragment();
     }
+
+    TextToSpeech tts;
+    TextView info_text;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.show_info_fragment, container, false);
         view.setBackgroundColor(getResources().getColor(R.color.backround));
+
+        //TODO:Text must be before
+        info_text = view.findViewById(R.id.info_text);
+
+
+        tts = new TextToSpeech(getActivity(), new TextToSpeech.OnInitListener() {
+
+            @Override
+            public void onInit(int status) {
+                if(status == TextToSpeech.SUCCESS){
+                    int result=tts.setLanguage(Locale.getDefault());
+                    tts.setSpeechRate((float)0.8);
+                    if(result==TextToSpeech.LANG_MISSING_DATA ||
+                            result==TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("error", "This Language is not supported");
+                    }
+                }
+                else
+                    Log.e("error", "Initilization Failed!");
+                tts.speak(info_text.getText().toString(),TextToSpeech.QUEUE_FLUSH, null,null); //To speak on init press
+
+            }
+        });
+
+
+
+
+
+
+        ImageButton replayButton = view.findViewById(R.id.replay_button);
+        replayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tts.speak(info_text.getText().toString(),TextToSpeech.QUEUE_FLUSH, null,null); //To speak on button press
+
+            }
+        });
+
         return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        tts.stop();
+    }
 }
